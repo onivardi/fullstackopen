7b402d023c7bfa30d087a911bbe7fde52e21cfa4
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2025-04-17 08:52:59 -0400

    exercise 1.2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -2,7 +2,13 @@ export const Content = (props) => {
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.course.parts[0].name} {props.course.parts[0].exercises}
+      </p>
+      <p>
+        {props.course.parts[1].name} {props.course.parts[1].exercises}
+      </p>
+      <p>
+        {props.course.parts[2].name} {props.course.parts[2].exercises}
       </p>
     </div>
   );
--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,10 +1,10 @@
 export const Total = (props) => {
+  const exercises1 = props.course.parts[0].exercises;
+  const exercises2 = props.course.parts[1].exercises;
+  const exercises3 = props.course.parts[2].exercises;
   return (
     <div>
-      <p>
-        Number of exercises{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
 };
